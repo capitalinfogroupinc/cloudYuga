bcbe3c42dc3f816459d31d05377fadd3dbbad203
cig <cig>
2016-10-31 18:12:57 -0700

    Angular and Express skeleton

--- server/app.js
+++ server/app.js
@@ -10,9 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,24 +23,6 @@ app.use(cookieParser());
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
-
-
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 /**
  * Development Settings
  */
@@ -63,6 +42,7 @@ if (app.get('env') === 'development') {
         });
     });
 }
+
 /**
  * Production Settings
  */
